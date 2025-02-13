805b5d6bc8c16fe166ba7381702f63b8af2f7f62
OjasJohar <ojasjohar@gmail.com>
2025-02-13 22:32:31 +0530

    [Ojas] Add. Completed Implement Banking System Problem

---
+++ Day02-Java-Collections-and-Streams/src/test/java/map_interface/implement_banking_system/Test_BankingSystem.java
@@ -0,0 +1,31 @@
+package map_interface.implement_banking_system;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test_BankingSystem{
+    private BankingSystem bank;
+
+    @BeforeEach
+    void setUp() {
+        bank = new BankingSystem();
+        bank.createAccount(1001, 500.0);
+        bank.createAccount(1002, 1500.0);
+        bank.createAccount(1003, 300.0);
+    }
+
+    @Test
+    void testDeposit() {
+        bank.deposit(1001, 200.0);
+        assertEquals(700.0, bank.getBalance(1001), "Deposit failed");
+    }
+
+    @Test
+    void testWithdrawalRequestAndProcessing() {
+        bank.requestWithdrawal(1001);
+        bank.processWithdrawals();
+        assertEquals(400.0, bank.getBalance(1001), "Withdrawal processing failed");
+    }
+}
