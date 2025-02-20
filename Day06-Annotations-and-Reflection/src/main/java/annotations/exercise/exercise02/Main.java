f05bab334d85c1de04ef5cfa4f28c9589ca7b486
OjasJohar <ojasjohar@gmail.com>
2025-02-20 11:15:53 +0530

    [Ojas] Add. completed program of use @Deprecated to mark an old method

---
+++ Day06-Annotations-and-Reflection/src/main/java/annotations/exercise/exercise02/Main.java
@@ -0,0 +1,12 @@
+package annotations.exercise.exercise02;
+import java.util.*;
+public class Main extends LegacyAPI {
+    public static void main(String[] args) {
+        LegacyAPI legacyAPI = new LegacyAPI();
+
+        //calling deprecated marked method
+        legacyAPI.oldFeature();
+        //calling new method
+        legacyAPI.newFeature();
+    }
+}
