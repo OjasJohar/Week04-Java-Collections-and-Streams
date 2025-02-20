8efc987ef816b2dd5a7fd9cee44a9f71d708aafb
OjasJohar <ojasjohar@gmail.com>
2025-02-20 11:31:53 +0530

    [Ojas] Add. completed program to create a @Todo annotation for pending tasks

---
+++ Day06-Annotations-and-Reflection/src/test/java/annotations_test/practiceproblems/basiclevelproblem/todoannotation/TodoAnnotationTest.java
@@ -0,0 +1,31 @@
+package annotations_test.practiceproblems.basiclevelproblem.todoannotation;
+import annotations.practiceproblems.beginnerlevel.todoannotation.*;
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.Method;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TodoAnnotationTest {
+    @Test
+    void testTask1Annotation() throws NoSuchMethodException {
+        Method method = TodoTask.class.getMethod("task1");
+
+        assertTrue(method.isAnnotationPresent(Todo.class), "Annotation should be present");
+
+        Todo annotation = method.getAnnotation(Todo.class);
+        assertEquals("Requirement gathering", annotation.task(), "Task should be Requirement gathering");
+        assertEquals("Sejal chauhan", annotation.assignedTo(), "AssignedTo should be Sejal chauhan");
+        assertEquals("High", annotation.priority(), "Priority should be High");
+    }
+
+    @Test
+    void testTask5Annotation() throws NoSuchMethodException {
+        Method method = TodoTask.class.getMethod("task5");
+
+        assertTrue(method.isAnnotationPresent(Todo.class), "Annotation should be present");
+
+        Todo annotation = method.getAnnotation(Todo.class);
+        assertEquals("checking software working", annotation.task(), "Task should be checking software working");
+        assertEquals("Om Tamrakar", annotation.assignedTo(), "AssignedTo should be Om Tamrakar");
+        assertEquals("High", annotation.priority(), "Priority should be High");
+    }
+}
